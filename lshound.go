aa0593c5d139427027c6e7498815aad764944871
Michael Cuomo <mcuomo777@gmail.com>
2025-11-02 12:46:34 -0500

    add functionality

--- lshound.go
+++ lshound.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
 	"os/user"
@@ -19,30 +18,29 @@ import (
 )
 
 type FileInfoRecord struct {
-	Path 				string		`json:"path"`
-	Type				string 		`json:"type"`
-	Mode 				string		`json:"mode"`
-	ModeOctal		string		`json:"mode_octal"`
-	UID					uint32		`json:"uid"`
-	GID					uint32		`json:"gid"`
-	User				string		`json:"user,omitempty"`
-	Group				string		`json:"group,omitempty"`
-	Size				int64			`json:"size"`
-	ModTime			time.Time	`json:"mod_time"`
-	IsSymlink		bool			`json:"is_symlink"`
-	LinkTarget	string		`json:"link_target,omitempty"`
-	ACL					bool			`json:"acl"`
-	Err					string		`json:"err,omitempty"`
+	Path       string    `json:"path"`
+	Type       string    `json:"type"`
+	Mode       string    `json:"mode"`
+	ModeOctal  string    `json:"mode_octal"`
+	UID        uint32    `json:"uid"`
+	GID        uint32    `json:"gid"`
+	User       string    `json:"user,omitempty"`
+	Group      string    `json:"group,omitempty"`
+	Size       int64     `json:"size"`
+	ModTime    time.Time `json:"mod_time"`
+	IsSymlink  bool      `json:"is_symlink"`
+	LinkTarget string    `json:"link_target,omitempty"`
+	ACL        bool      `json:"acl"`
+	Err        string    `json:"err,omitempty"`
 }
 
 var (
-	startPath			string
-	doJSON				bool
-	checkACL			bool
-	followSymlink	bool
-	maxDepth			int
+	startPath     string
+	doJSON        bool
+	checkACL      bool
+	followSymlink bool
+	maxDepth      int
 )
-
 
 func modeToStirng(m os.FileMode) string {
 	var b [9]byte
@@ -81,8 +79,8 @@ func modeToStirng(m os.FileMode) string {
 }
 
 func uidToUser(uid uint32) string {
-	u, err := user.LookupId(strconv.FormatUnit(uint64(uid), 10))
-	if err = nil {
+	u, err := user.LookupId(strconv.FormatUint(uint64(uid), 10))
+	if err == nil {
 		return u.Username
 	}
 
@@ -95,7 +93,7 @@ func uidToUser(uid uint32) string {
 	uidStr := strconv.FormatUint(uint64(uid), 10)
 	for sc.Scan() {
 		line := sc.Text()
-		if line == "" | strings.HasPrefix(line "#") {
+		if line == "" || strings.HasPrefix(line, "#") {
 			continue
 		}
 		parts := strings.Split(line, ":")
@@ -104,4 +102,193 @@ func uidToUser(uid uint32) string {
 		}
 	}
 	return ""
+}
+
+func gidToGroup(gid uint32) string {
+	g, err := user.LookupGroupId(strconv.FormatUint(uint64(gid), 10))
+	if err == nil {
+		return g.Name
+	}
+	f, ferr := os.Open("/etc/group")
+	if ferr != nil {
+		return ""
+	}
+	defer f.Close()
+	sc := bufio.NewScanner(f)
+	gidStr := strconv.FormatUint(uint64(gid), 10)
+	for sc.Scan() {
+		line := sc.Text()
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+		parts := strings.Split(line, ":")
+		if len(parts) >= 3 && parts[2] == gidStr {
+			return parts[0]
+		}
+	}
+	return ""
+}
+
+func detectACL(path string) (bool, error) {
+	_, err := exec.LookPath("getfacl")
+	if err != nil {
+		return false, nil
+	}
+
+	cmd := exec.Command("getfacl", "p", path)
+	out, err := cmd.Output()
+	if err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			out = append(out, ee.Stderr...)
+		} else {
+			return false, err
+		}
+	}
+
+	scanner := bufio.NewScanner(bytes.NewReader(out))
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+		if strings.HasPrefix(line, "#") || line == "" {
+			continue
+		}
+		if strings.HasPrefix(line, "user:") {
+			parts := strings.Split(line, ":")
+			if len(parts) >= 3 && parts[1] != "" {
+				if parts[1] != "" && parts[1] != "0" {
+					if parts[1] != "" {
+						return true, nil
+					}
+				}
+			}
+		}
+		if strings.HasPrefix(line, "group:") {
+			parts := strings.Split(line, ":")
+			if len(parts) >= 3 && parts[1] != "" {
+				return true, nil
+			}
+		}
+		if strings.HasPrefix(line, "mask:") || strings.HasPrefix(line, "default:") {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func processPath(path string, info os.FileInfo) FileInfoRecord {
+	rec := FileInfoRecord{
+		Path:    path,
+		Size:    info.Size(),
+		ModTime: info.ModTime(),
+	}
+	mode := info.Mode()
+	rec.Mode = modeToStirng(mode)
+	rec.ModeOctal = fmt.Sprintf("%#o", uint32(mode.Perm()))
+	rec.IsSymlink = (mode & os.ModeSymlink) != 0
+	if rec.IsSymlink {
+		rec.Type = "syslink"
+		if tgt, err := os.Readlink(path); err == nil {
+			rec.LinkTarget = tgt
+		}
+	} else if mode.IsDir() {
+		rec.Type = "dir"
+	} else if mode.IsRegular() {
+		rec.Type = "file"
+	} else {
+		rec.Type = "other"
+	}
+
+	var stat syscall.Stat_t
+	if err := syscall.Lstat(path, &stat); err == nil {
+		rec.UID = uint32(stat.Uid)
+		rec.GID = uint32(stat.Gid)
+		rec.User = uidToUser(rec.UID)
+		rec.Group = gidToGroup(rec.GID)
+	} else {
+		rec.Err = err.Error()
+	}
+
+	if checkACL {
+		acl, err := detectACL(path)
+		if err != nil {
+			if rec.Err == "" {
+				rec.Err = err.Error()
+			} else {
+				rec.Err = rec.Err + "; " + err.Error()
+			}
+		}
+		rec.ACL = acl
+	}
+
+	return rec
+}
+
+func walk(root string) error {
+	rootAbs, err := filepath.Abs(root)
+	if err == nil {
+		root = rootAbs
+	}
+	rootDepth := len(strings.Split(filepath.Clean(root), string(os.PathSeparator)))
+
+	return filepath.WalkDir(root, func(path string, dEntry os.DirEntry, err error) error {
+		if err != nil {
+			rec := FileInfoRecord{Path: path, Err: err.Error()}
+			emit(rec)
+			return nil
+		}
+
+		if maxDepth >= 0 {
+			curDepth := len(strings.Split(filepath.Clean(path), string(os.PathSeparator)))
+			if curDepth-rootDepth > maxDepth {
+				if dEntry.IsDir() {
+					return filepath.SkipDir
+				}
+				return nil
+			}
+		}
+
+		var info os.FileInfo
+		if followSymlink {
+			info, err = os.Stat(path)
+		} else {
+			info, err = os.Lstat(path)
+		}
+		if err != nil {
+			rec := FileInfoRecord{Path: path, Err: err.Error()}
+			emit(rec)
+			return nil
+		}
+		rec := processPath(path, info)
+		emit(rec)
+		return nil
+	})
+}
+
+func emit(rec FileInfoRecord) {
+	if doJSON {
+		js, _ := json.Marshal(rec)
+		fmt.Println(string(js))
+	} else {
+		fmt.Printf("%s\t%s\t%s\tuid:%d\tgid:%d\tuser:%s\tgroup:%s\tsize:%d\tacl:%t\n",
+			rec.Path, rec.Type, rec.Mode, rec.UID, rec.GID, rec.User, rec.Group, rec.Size, rec.ACL)
+	}
+}
+
+func init() {
+	flag.StringVar(&startPath, "path", ".", "starting path")
+	flag.BoolVar(&doJSON, "json", true, "output to json")
+	flag.BoolVar(&checkACL, "acl", false, "check for POSIX ACLs using getfacl (if available)")
+	flag.BoolVar(&followSymlink, "follow-symlink", false, "follow symlink when stat'ing files")
+	flag.IntVar(&maxDepth, "max-depth", -1, "max recursive depth relative to start (-1 = unlimited)")
+}
+
+func main() {
+	flag.Parse()
+	if startPath == "" {
+		startPath = "."
+	}
+	if err := walk(startPath); err != nil {
+		fmt.Fprintln(os.Stderr, "walk error: ", err)
+		os.Exit(1)
+	}
 }
